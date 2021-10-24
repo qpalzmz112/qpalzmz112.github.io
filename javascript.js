b17520b58610c5284ec037ce8947618daf4c2399
qpalzmz112 <68213464+qpalzmz112@users.noreply.github.com>
2021-10-24 18:26:48 -0500

    adding undo reverse

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+let alreadyCalled = false;
+
 document.onselectionchange = () => {
     handleSelectionChange();
 }
@@ -15,19 +17,50 @@ function handleSelectionChange() {
     }
 }
 
-
 function changeTextSpacing() {
     let string = window.getSelection().toString();
     document.getElementById("textSpacing").style.letterSpacing = string.length/2 + "px"
 }
 
+let originalSelections = [];
+let reversedSelections = [];
 function reverseText() {
     let text = document.getElementById("textReverse").innerHTML;
     let originalSelection = window.getSelection().toString();
+    originalSelections.unshift(originalSelection);
+    if (originalSelection.length == 1) {
+        return;
+    }
     let selectionCopy = window.getSelection().toString();
     selectionCopy = selectionCopy.split("");
     selectionCopy = selectionCopy.reverse();
     selectionCopy = selectionCopy.join();
     selectionCopy = selectionCopy.replaceAll(',', '');
+    reversedSelections.unshift(selectionCopy);
     document.getElementById("textReverse").innerHTML = text.replace(originalSelection, selectionCopy);
+    if (!alreadyCalled) {
+        undoReverseText();
+    }
+    
+}
+
+function undoReverseText() {
+    const element = document.getElementById("textReverse");
+    alreadyCalled = true;
+    if (window.getSelection().isCollapsed) {
+        while (originalSelections.length > 0) {
+            sleep(500).then(() => {
+                element.innerHTML = text.replace(originalSelections.shift(), reversedSelections.shift());
+            })
+        }
+        
+    }
+    else {
+        setTimeout(undoReverseText, 1000);
+    }
+    alreadyCalled = false;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
