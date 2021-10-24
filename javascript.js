a37fc80acbf48ecc8e77a2ed63e02d0e8485c301
qpalzmz112 <68213464+qpalzmz112@users.noreply.github.com>
2021-10-24 01:56:28 -0500

    adding first selection function

--- javascript.js
+++ javascript.js
@@ -5,15 +5,19 @@ document.onselectionchange = () => {
 
 function handleSelectionChange() {
     let selection = window.getSelection();
-    let selectedElement = selection.parentElement;
-    console.log(selectedElement.id);
-    console.log("a");
+    let selectedElement = selection.focusNode.parentElement;
+    switch (selectedElement.id) {
+        case "textSpacing":
+            changeTextSpacing();
+            break;
+        case "textReverse":
+            reverseText();
+            break;
+    }
 }
-
-
 
 
 function changeTextSpacing() {
     let string = window.getSelection().toString();
-    document.getElementById("textSpacing").style.letterSpacing = string.length() + "px"
+    document.getElementById("textSpacing").style.letterSpacing = string.length()/2 + "px"
 }
