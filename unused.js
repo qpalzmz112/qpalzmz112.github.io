1c0843c19068b635f3211917394c6332f3527254
qpalzmz112 <68213464+qpalzmz112@users.noreply.github.com>
2021-10-25 18:21:11 -0500

    adding background gradient

--- unused.js
+++ unused.js
@@ -8,6 +8,27 @@
     //if (!alreadyCalled) {
         //undoReverseText();
 
+/* HTML and CSS for background image
+* {
+    z- index: 1;
+}
+#backgroundImage {
+    z - index: 0;
+    opacity: 60 %;
+    position: relative;
+    width: 100 %;
+    height: 100 %;
+}
+.imageSource {
+    position: absolute;
+    bottom: 0;
+    ri
+        < div id = "backgroundImage" >
+            <img src="/aerial cityscape(1440p).jpeg" />
+            <a class="imageSource" href="https://www.pexels.com/photo/bird-s-eye-view-of-city-during-dawn-2093323/" >Image Source</a>
+        </div >
+*/
+
 function undoReverseText() {
     const element = document.getElementById("textReverse");
     alreadyCalled = true;
