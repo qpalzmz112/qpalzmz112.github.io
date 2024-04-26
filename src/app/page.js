a071e4fa4cf240ab91c072df11c5b496dc4f4e6f
qpalzmz112 <68213464+qpalzmz112@users.noreply.github.com>
2024-04-25 20:34:21 -0500

    Updated home page

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <Boilerplate>
-      <div className="h-max flex justify-center">
-        <div className="pt-40 pr-24">
-          <h1 className="text-7xl font-medium pl-3 pt-1">Sean Carlson</h1>
+      <div className="grid grid-cols-2 place-items-center gap-4 p-4">
+        <div>
+          <h1 className="text-7xl font-medium pl-3">Sean Carlson</h1>
           <h3 className="text-3xl text-gray-500 font-medium pl-3">
             Software Engineer
           </h3>
@@ -17,7 +17,7 @@ export default function Home() {
             the best user experience possible.
           </p>
         </div>
-        <div className="pt-20">
+        <div>
           <Image
             className="rounded-full"
             src="/headshot.jpg"
