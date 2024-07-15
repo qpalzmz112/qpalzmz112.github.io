6051c997bd33295c3fc6a19cbe33c9571d3cadab
qpalzmz112 <68213464+qpalzmz112@users.noreply.github.com>
2024-07-14 23:11:24 -0500

    update headshot

--- src/app/page.js
+++ src/app/page.js
@@ -7,13 +7,11 @@ export default function Home() {
       <div className="grid grid-cols-2 place-items-center gap-4 p-4">
         <div>
           <h1 className="text-7xl font-medium pl-3">Sean Carlson</h1>
-  {/*<h3 className="text-3xl text-gray-500 font-medium pl-3">
-            Software Engineer
-          </h3>*/}
           <p className="p-3 max-w-2xl text-lg">
-            I graduated from Northwestern University with a double major
-            in computer science and math. I enjoy writing software and find satisfaction
-            in paying attention to as much detail as possible when doing so.
+            I graduated from Northwestern University with a double major in
+            computer science and math. I enjoy writing software and find
+            satisfaction in paying attention to as much detail as possible when
+            doing so.
           </p>
         </div>
         <div>
